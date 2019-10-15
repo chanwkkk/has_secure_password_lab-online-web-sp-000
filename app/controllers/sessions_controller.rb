03c31c6dfe7bfc42c4472fc1379c3a51d4a0073d
Yingqi Chen <chanwk18@hotmail.com>
2019-10-15 22:58:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     binding.pry
-    @user=User.find_by name: params[:name]
+    @user=User.find_by name: params[:user][:name]
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect_to user_path(@user)
